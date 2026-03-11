8c4d6f0beb5fa773829a60099fe8b2c9371b50c2
capybara <1375283285@qq.com>
2026-03-12 01:30:16 +0800

    添加 logback-weblog.xml 文件以整合 Logback 到后端工程；添加测试方法测试日志时候正确添加。

--- weblog-web/src/test/java/com/capybara349/weblogweb/WeblogWebApplicationTests.java
+++ weblog-web/src/test/java/com/capybara349/weblogweb/WeblogWebApplicationTests.java
@@ -1,13 +1,23 @@
 package com.capybara349.weblogweb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class WeblogWebApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testLog() {
+        log.info("Hello World");
+        log.warn("Hello World");
+        log.error("Hello World");
+        log.info("Hello World{}", "!");
     }
 
 }
