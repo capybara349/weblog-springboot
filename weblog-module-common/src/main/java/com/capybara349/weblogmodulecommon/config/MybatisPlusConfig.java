00cd31a00cc1b5677b6df8f776a2e7f7adad0143
capybara <1375283285@qq.com>
2026-03-13 11:02:06 +0800

    使用 Mybatis-plus 插件和 p6spy 组件打印 SQL 日志与耗时。

---
+++ weblog-module-common/src/main/java/com/capybara349/weblogmodulecommon/config/MybatisPlusConfig.java
@@ -0,0 +1,14 @@
+package com.capybara349.weblogmodulecommon.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * &#064;author capybara349 </br>
+ * &#064;date 2026.03.13 10:40
+ */
+@Configuration
+@MapperScan("com.capybara349.weblogmodulecommon.domain.mapper")
+public class MybatisPlusConfig {
+}
--- weblog-web/src/test/java/com/capybara349/weblogweb/WeblogWebApplicationTests.java
+++ weblog-web/src/test/java/com/capybara349/weblogweb/WeblogWebApplicationTests.java
@@ -1,12 +1,20 @@
 package com.capybara349.weblogweb;
 
+import com.capybara349.weblogmodulecommon.domain.dos.UserDO;
+import com.capybara349.weblogmodulecommon.domain.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 @Slf4j
 class WeblogWebApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
@@ -20,4 +28,21 @@ class WeblogWebApplicationTests {
         log.info("Hello World{}", "!");
     }
 
+
+    @Test
+    void insertTest() {
+        UserDO userDO = UserDO.builder()
+                .username("capybara349")
+                .password("123456")
+                .createTime(new Date())
+                .updateTime(new Date())
+                .isDeleted(false)
+                .build();
+        userMapper.insert(userDO);
+    }
+
+    @Test
+    void selectTest() {
+        userMapper.selectById(1);
+    }
 }
